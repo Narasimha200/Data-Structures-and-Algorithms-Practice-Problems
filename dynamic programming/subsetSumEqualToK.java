a26ee06aae09dcb409769f59ee6a17a8d401d598
narasimha <laxminarasimhavadla2004@gmail.com>
2024-11-10 11:40:39 +0530

    dp

--- dynamic programming/subsetSumEqualToK.java
+++ dynamic programming/subsetSumEqualToK.java
@@ -1,31 +1,52 @@
 //Problem Link:- "https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1"
 public class subsetSumEqualToK {
     class Solution {
-    
-        static Boolean check(int idx, int arr[], int target, int[][] dp){
-            if(target == 0) return true;
-            if(idx == 0) return arr[0] == target;
-            if(dp[idx][target] != -1) return dp[idx][target] == 1;
-            Boolean pick = check(idx - 1, arr, target, dp);
-            Boolean notPick = false;
-            if(arr[idx] <= target){
-                notPick = check(idx -1, arr, target - arr[idx], dp);
+        // memoization
+        static Boolean check(int idx, int arr[], int target, int[][] dp) {
+            if (target == 0)
+                return true;
+            if (idx == 0)
+                return arr[0] == target;
+            if (dp[idx][target] != -1)
+                return dp[idx][target] == 1;
+            Boolean notPick = check(idx - 1, arr, target, dp);
+            Boolean pick = false;
+            if (arr[idx] <= target) {
+                pick = check(idx - 1, arr, target - arr[idx], dp);
             }
-            dp[idx][target] = (pick || notPick)? 1: 0;
+            dp[idx][target] = (pick || notPick) ? 1 : 0;
             return dp[idx][target] == 1;
         }
-        
+        // TC:- (N * sum)
+        // SC:- (N * sum) + O(N)
+
+        // tabulation with space optimization
         static Boolean isSubsetSum(int arr[], int sum) {
             int n = arr.length;
-            int[][] dp = new int[n][sum+1];
-            for(int i = 0; i < dp.length; i++){
-                for(int j = 0; j < dp[0].length; j++){
-                    dp[i][j] = -1;
+            boolean[] prev = new boolean[sum + 1];
+            prev[0] = true;
+            if (arr[0] <= sum)
+                prev[arr[0]] = true;
+            boolean[] curr = new boolean[sum + 1];
+            curr[0] = true;
+            for (int idx = 1; idx < n; idx++) {
+                for (int target = 0; target <= sum; target++) {
+                    boolean notPick = prev[target];
+                    Boolean pick = false;
+                    if (arr[idx] <= target){
+                        pick = prev[target - arr[idx]];
+                    }
+                    curr[target] = notPick || pick;
                 }
+                boolean[] temp = prev;
+                prev = curr;
+                curr = temp;
             }
-            return check(n-1, arr, sum, dp);
+            return prev[sum];
         }
+        //TC:- O(N * sum)
+        //SC:- O(sum)
     }
 }
-//TC:- (N * sum)
-//SC:- (N * sum) + O(N)
+// TC:- (N * sum)
+// SC:- (N * sum) + O(N)
